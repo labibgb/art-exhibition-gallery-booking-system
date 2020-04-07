a842fd72b73e501a692c166f2bb14276a932bdbe
labibgb <labibgb@gmail.com>
2020-04-08 03:01:00 +0600

    Update models and modify admin

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Gallery, Location,ServiceList, Services, Feedback
 
-# Register your models here.
+admin.site.register( Gallery )
+admin.site.register( Location )
+admin.site.register( ServiceList )
+admin.site.register( Services )
+admin.site.register( Feedback )
--- galleries/models.py
+++ galleries/models.py
@@ -1,2 +1,47 @@
 from django.db import models
 
+class Location( models.Model ):
+    
+    location = models.CharField( max_length=200 )
+
+    def __str__( self ):
+        return self.location
+
+class Gallery( models.Model ):
+
+    location = models.ForeignKey( Location , on_delete=models.DO_NOTHING )
+    name = models.CharField( max_length=200 )
+    description = models.TextField( blank= True )
+    img_1 = models.ImageField(upload_to='photo/%y/%m/%d/', blank=True)
+    img_2 = models.ImageField(upload_to='photo/%y/%m/%d/', blank=True)
+    img_3 = models.ImageField(upload_to='photo/%y/%m/%d/', blank=True)
+    img_4 = models.ImageField(upload_to='photo/%y/%m/%d/', blank=True)
+    img_4 = models.ImageField(upload_to='photo/%y/%m/%d/', blank=True)
+    is_available = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__( self ):
+        return self.name
+
+class ServiceList( models.Model ):
+    serviceName = models.CharField( max_length=20 , blank=True )
+    price = models.IntegerField(default=0)
+
+    def __str__( self ):
+        return self.serviceName
+
+class Services( models.Model ):
+
+    servicelist = models.ForeignKey( ServiceList , on_delete=models.DO_NOTHING )
+    gallery = models.ForeignKey( Gallery , on_delete=models.DO_NOTHING )
+
+    def __str__( self ):
+        return self.gallery.name
+
+class Feedback( models.Model ):
+
+    gallery = models.ForeignKey( Gallery , on_delete=models.DO_NOTHING )
+    feedback = models.TextField( blank=True )
+
+    def __str__( self ):
+        return self.gallery.name
