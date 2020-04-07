5b174f5b99f66f77b4f8d247a2a9e8b9c63bd53f
labibgb <labibgb@gmail.com>
2020-04-08 02:57:52 +0600

    Update admin file and modify admin page

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking
+
+class bookingAdmin( admin.ModelAdmin ):
+    list_display = ('id' , 'gallery', 'booking_date', 'end_date' )
+    list_display_links = ( 'id' , 'gallery')
+    list_filter = ( 'gallery',)
+    #list_editable = (, , )
+    #search_fields = ( , , )
+    list_per_page = 20
+
+admin.site.register( Booking , bookingAdmin)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
+from galleries.models import Gallery
 
-# Create your models here.
+class Booking( models.Model ):
+
+    gallery = models.ForeignKey( Gallery , on_delete = models.DO_NOTHING )
+    booking_date = models.DateField( default = datetime.now , blank = True)
+    end_date = models.DateField( default = datetime.now , blank = True )
+    def __str__( self ):
+        return self.gallery.name
