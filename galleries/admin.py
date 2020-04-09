df051d507f1688481df72a2be556544d8f036a2a
labibgb <labibgb@gmail.com>
2020-04-09 19:43:43 +0600

    Update admin list_item

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Gallery, Location,ServiceList, Services, Feedback
 class galleyAdmin( admin.ModelAdmin ):
-    list_display = ( 'id' , 'name', 'description', 'location' , 'is_available' , 'is_published' )
-    list_display_links = ( 'id', )
+    list_display = ( 'id' , 'name', 'location' , 'is_available' , 'is_published' )
+    list_display_links = ( 'id', 'name', )
     list_filter = ( 'is_published' , 'location' , 'is_available')
-    list_editable = ('name', 'description', 'location' , 'is_available' , 'is_published' )
+    list_editable = ( 'location' , 'is_available' , 'is_published' )
     list_per_page = 20
 
 admin.site.register( Gallery , galleyAdmin )
