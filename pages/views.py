837e72afa14d4d5617f6c5f929904cfe1cb253eb
labibgb <labibgb@gmail.com>
2020-04-09 18:34:31 +0600

    Make home page dynamic

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from galleries.models import Gallery
 def home( request ):
-    return render( request , 'pages/index.html')
+    gallery = Gallery.objects.order_by('name').filter(is_published=True).filter(is_available=True)[:3]
+    context = {
+        'galleries' : gallery
+    }
+    return render( request , 'pages/index.html', context )
 
 def about( request ):
     return render( request , 'pages/about.html')
